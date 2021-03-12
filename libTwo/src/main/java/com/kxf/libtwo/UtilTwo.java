5e21650ad58ae767d37be807496cbfbf02eef730
kxf <1024883177@qq.com>
2021-03-12 14:14:26 +0800

    完成功能，合并包完成

---
+++ libTwo/src/main/java/com/kxf/libtwo/UtilTwo.java
@@ -0,0 +1,16 @@
+package com.kxf.libtwo;
+
+/**
+ * @ProjectName: FatAarTest
+ * @Package: com.kxf.libtwo
+ * @ClassName: UtilTwo
+ * @Description: java类作用描述
+ * @Author: kuangxuefeng
+ * @qq: 1024883177
+ * @CreateDate: 2021/3/12 14:02
+ */
+public class UtilTwo {
+    public static String getInfo(){
+        return "我是 UtilTwo";
+    }
+}
